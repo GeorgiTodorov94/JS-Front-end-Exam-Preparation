d6cc7b74b08073dd83bbbd257f32dabc8397015c
Your Name <you@example.com>
2024-07-17 19:59:27 +0300

    Install & Setup React Router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
-
+import { BrowserRouter } from 'react-router-doms'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <BrowserRouter>
+    <App />
+  </BrowserRouter >
 )
