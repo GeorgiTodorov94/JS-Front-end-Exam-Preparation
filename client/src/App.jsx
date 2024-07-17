ac71a069d9360101cf38681aed82462a0050f03f
Your Name <you@example.com>
2024-07-17 19:09:32 +0300

    Clean Up React Project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,12 @@
 
-import './App.css'
 
 function App() {
 
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <>
+            <h1>Games Play</h1>
+        </>
+    )
 }
 
 export default App
